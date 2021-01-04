af7a3875d30eb34bd15ebd09ebcd60793d088deb
Natalia Ilina <ilinans@msk.bcs.ru>
2021-01-05 01:11:24 +0300

    add hover effects

--- src/components/Button/Button.styled.ts
+++ src/components/Button/Button.styled.ts
@@ -5,9 +5,9 @@ export const SButton = styled.button`
   margin: 0;
   outline: 0;
   background-color: transparent;
-  border: 1px solid ${(props) => props.theme.colors.text.primary};
-  color: ${(props) => props.theme.colors.text.primary};
-  font-family: ${(props) => props.theme.fontFamily.default};
+  border: 1px solid ${({ theme }) => theme.colors.common.yellow};
+  color: ${({ theme }) => theme.colors.text.primary};
+  font-family: ${({ theme }) => theme.fontFamily.default};
   font-size: 14px;
   line-height: 1.2;
   font-weight: 400;
@@ -15,4 +15,11 @@ export const SButton = styled.button`
   letter-spacing: -0.025em;
   border-radius: 4px;
   cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:hover,
+  &:focus {
+    background-color: ${({ theme }) => theme.colors.common.brown};
+    box-shadow: ${({ theme }) => theme.boxShadow.brown};
+  }
 `;
--- src/components/NavigationLink/NavigationLink.styled.ts
+++ src/components/NavigationLink/NavigationLink.styled.ts
@@ -1,6 +1,21 @@
 import styled from 'styled-components';
 import Text from '@/components/Text/Text';
+import { decoration } from '@/styled/common';
 
 export const SNavigationLink = styled(Text.link)`
   padding: 12px 0;
+  position: relative;
+
+  &::before {
+    ${decoration};
+    bottom: 0;
+    left: 0;
+    width: 0;
+    box-shadow: none;
+  }
+
+  &:hover::before {
+    width: 70px;
+    box-shadow: ${({ theme }) => theme.boxShadow.white};
+  }
 `;
