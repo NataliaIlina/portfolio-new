c8eb038c54c05d7c6bc65e04f72e559b192b8762
Natalia Ilina <ilinans@msk.bcs.ru>
2021-01-05 17:51:30 +0300

    fix svg ids

--- src/components/icons/contacts/Telegram.tsx
+++ src/components/icons/contacts/Telegram.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const Telegram: React.FC = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <path
       d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24Z"
-      fill="url(#paint0_linear)"
+      fill="url(#telegram_1)"
     />
     <path
       d="M9.79995 17.5C9.41119 17.5 9.47725 17.3532 9.34317 16.9831L8.19995 13.2206L16.9999 8"
@@ -18,11 +24,11 @@ const Telegram: React.FC = () => (
     />
     <path
       d="M10.004 14.441L14.84 18.0139C15.3918 18.3184 15.7901 18.1607 15.9276 17.5016L17.8961 8.22525C18.0976 7.41723 17.5881 7.05075 17.0601 7.29043L5.50113 11.7475C4.71212 12.064 4.71672 12.5042 5.35731 12.7003L8.32361 13.6262L15.1909 9.29366C15.5151 9.09707 15.8126 9.20276 15.5684 9.4195"
-      fill="url(#paint1_linear)"
+      fill="url(#telegram_2)"
     />
     <defs>
       <linearGradient
-        id="paint0_linear"
+        id="telegram_1"
         x1="16.0008"
         y1="4.0008"
         x2="10.0008"
@@ -33,7 +39,7 @@ const Telegram: React.FC = () => (
         <stop offset="1" stopColor="#928653" />
       </linearGradient>
       <linearGradient
-        id="paint1_linear"
+        id="telegram_2"
         x1="13.5005"
         y1="11.9999"
         x2="15.3784"
